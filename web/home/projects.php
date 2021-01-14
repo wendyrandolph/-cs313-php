117478a261c58804cb5e0b399ba81fc1821650f9
wabbitslayer <wabbitslayer24@gmail.com>
2021-01-14 16:44:04 -0700

    update

--- web/home/projects.php
+++ web/home/projects.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projects</title>
-    <link rel="stylesheet" type="text/css" href="../css/styles.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../home/css/styles.css" media="screen" />
 </head>
 
 <body>
