006e2fb52db7c9077da6f834f86bc709616c4d00
wabbitslayer <wabbitslayer24@gmail.com>
2021-01-14 16:45:43 -0700

    index.php update

--- web/home/index.php
+++ web/home/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Me</title>
-    <link rel="stylesheet" type="text/css" href="../css/styles.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../home/css/styles.css" media="screen" />
 </head>
 <body>
     
