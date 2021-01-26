df4dbd5cb3264fadd1c3b6cb90fdf207e35e2c97
wabbitslayer <wabbitslayer24@gmail.com>
2021-01-25 17:19:13 -0700

    week_3 index.php update

--- web/home/index.php
+++ web/home/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Me</title>
-    <link rel="stylesheet" type="text/css" href="/css/styles.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="/css/nav.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/styles.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/nav.css" media="screen" />
 </head>
 <body>
     
