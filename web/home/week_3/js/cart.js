a1cb53ad62e0991c97046f81f466303643c6a1d4
wabbitslayer <wabbitslayer24@gmail.com>
2021-01-27 01:01:36 -0700

    reupload

---
+++ web/home/week_3/js/cart.js
@@ -0,0 +1,39 @@
+//increment the quantity of the products 
+
+//btnAdd = document.querySelector('#add');
+//btnMinus = document.querySelector('#minus');
+//input = document.querySelector('#pId');
+//variable for grabbing the add to cart button element 
+addbutton = document.querySelector('#addme'); 
+/*
+//btnAdd.addEventListener('click', () => {
+ //   pId.value = parseInt(pId.value) + 1;
+
+//})
+
+btnMinus.addEventListener('click', () => {
+    if (pId.value > 1) {
+        pId.value = parseInt(pId.value) - 1;
+    } else {
+        pId.value = 0;
+    }
+})
+
+*/
+function addtocart(){ 
+
+    
+addbutton.addEventListener('click', () => { 
+
+    alert("THIS IS THE add to cart function"); 
+
+
+}); 
+
+}
+
+
+
+
+
+
---
+++ web/home/week_3/view/cart.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>View Cart</title>
+
+  <link rel="stylesheet" type="text/css" href="/css/styles.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="/css/nav.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="/css/week_3.css" media="screen" />
+
+</head>
+
+<body>
+
+  <header class="header">
+    <?php require $_SERVER['DOCUMENT_ROOT'] . '/snippets/header.php'; ?>
+    <nav>
+      <?php require $_SERVER['DOCUMENT_ROOT'] . '/snippets/nav.php'; ?>
+    </nav>
+  </header>
+  <div class="page-title">
+    <h2> Shopping Cart </h2>
+    <nav class="cart">
+      <ul>
+        <li><a href='/week_3/index.php'>Home</a></li>
+        <li><a href='/week_3/index.php?action=viewCart'>View Cart</a></li>
+        <li><a href='/week_3/index.php?action=clearCart'>Clear Cart</a></li>
+        <input type="hidden" name="clear" value="1">
+        <ul>
+    </nav>
+  </div>
+
+  <div>
+    <?php if (isset($_SESSION['message_clear'])) {
+      echo $_SESSION['message_clear'];
+    } ?>
+  </div>
+  <main class="browse">
+
+    <div class="display">
+
+   <?php if(isset($view_cart)){ 
+     echo $view_cart; 
+   } ?> 
+
+
+    <div class="checkout">
+      <a href='/week_3/index.php?action=checkout'>Check Out</a>
+    </div>
+  </main>
+  <footer>
+    <div class="footer">
+      <?php require $_SERVER['DOCUMENT_ROOT'] . '/snippets/footer.php'; ?>
+    </div>
+  </footer>
+
+</body>
+
+
+</html>
